57d7730ab4ba1335836ced659babbb199541f59b
Sune Debel <sune.debel@leoilab.com>
2021-08-30 13:04:29 +0200

    fix filter_sales_year

--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 """Utilities for analyzing sales prices."""
 
-from pathlib import Path
 from datetime import datetime
 
 import pandas
@@ -19,17 +18,18 @@ def count_sales_year(df: pandas.DataFrame) -> pandas.Series:
 def filter_sales_year(
     df: pandas.DataFrame,
     min_sales_year: int = 0,
-    max_sales_year: int = datetime.today().year + 1
+    max_sales_year: int = datetime.today().year
 ) -> pandas.DataFrame:
     """Filter sales by sales year."""
-    idx = (df.date > str(min_sales_year)) & (df.date < str(max_sales_year))
+    idx = (df.date > str(min_sales_year)) & (df.date < str(max_sales_year + 1))
     return df[idx]
 
 
 def filter_build_year(
     df: pandas.DataFrame,
     min_build_year: int = 0,
-    max_build_year: int = datetime.today().year + 1
+    max_build_year: int = datetime.today().year
 ) -> pandas.DataFrame:
     """Filter sales by build year."""
-    return df[(df.built > min_build_year) & (df.built < max_build_year)]
+    idx = (df.built > min_build_year - 1) & (df.built < max_build_year + 1)
+    return df[idx]
