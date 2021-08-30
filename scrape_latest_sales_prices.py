cbad77a23cc0823494449209e3a9c717aee1485a
Sune Debel <sune.debel@leoilab.com>
2021-08-30 14:26:26 +0200

    formatting

--- scrape_latest_sales_prices.py
+++ scrape_latest_sales_prices.py
@@ -8,11 +8,13 @@ import logging
 from pathlib import Path
 
 import requests
-import bs4
-import pandas
+import bs4  # type: ignore
+import pandas  # type: ignore
 
 
 class Row(TypedDict):
+    """A row of sales price data."""
+
     address: str
     zip: str
     price: float
@@ -23,8 +25,9 @@ class Row(TypedDict):
     m2_price: float
 
 
-
 class NoSoldListError(Exception):
+    """Error used when the boliga response contains no sold list."""
+
     pass
 
 
@@ -35,6 +38,7 @@ address_pattern = (r'(?P<street>[a-zA-ZæøåÆØÅ ]+) '
 
 
 def scrape_prices(soup: bs4.BeautifulSoup) -> List[Row]:
+    """Scrape all sales prices from the sold table in a boliga response.."""
     if not soup.find_all('app-sold-list-table'):
         raise NoSoldListError()
     table = soup.find_all('app-sold-list-table')[0].table
@@ -65,11 +69,13 @@ def scrape_prices(soup: bs4.BeautifulSoup) -> List[Row]:
 
 
 def scrape_year(columns: bs4.element.ResultSet) -> str:
+    """Scrape the build year from a row in the boliga sold table."""
     year = columns[5].find_all('span', {'class': None})[0].text.strip()
     return year
 
 
 def scrape_area(columns: bs4.element.ResultSet) -> str:
+    """Scrape the area in m2 from a row in the boliga sold table."""
     area = columns[3].find('span').text.strip()
     m = re.match(r'\d+', area)
     if m is None:
@@ -79,11 +85,13 @@ def scrape_area(columns: bs4.element.ResultSet) -> str:
 
 
 def scrape_rooms(columns: bs4.element.ResultSet) -> str:
+    """Scrape the no. of rooms from a row in the boliga sold table."""
     rooms = columns[4].text.strip()
     return rooms
 
 
 def scrape_date(columns: bs4.element.ResultSet) -> str:
+    """Scrape the sales date from a row in the boliga sold table."""
     date = columns[2].find_all(
         'span',
         {'class': 'text-nowrap'}
@@ -92,6 +100,7 @@ def scrape_date(columns: bs4.element.ResultSet) -> str:
 
 
 def scrape_price(columns: bs4.element.ResultSet) -> str:
+    """Scrape the sales price from a row in the boliga sold table."""
     price = columns[1].find_all(
         'span',
         {'class': 'text-nowrap'}
@@ -107,8 +116,9 @@ def scrape_price(columns: bs4.element.ResultSet) -> str:
     ).strip()
     return price
 
-    
+
 def match_address(columns: bs4.element.ResultSet) -> re.Match:
+    """Scrape the address from a sold table row and apply address regex."""
     address = columns[0].find(attrs={'data-gtm': 'sales_address'}).text.strip()
     m = re.match(address_pattern, address)
     if m is None:
@@ -117,32 +127,37 @@ def match_address(columns: bs4.element.ResultSet) -> re.Match:
 
 
 def scrape_street(columns: bs4.element.ResultSet) -> str:
+    """Scrape the street name and no. from a row in the boliga sold table."""
     m = match_address(columns)
     street = f'{m.group("street")} {m.group("number")}'
     return street
 
 
 def scrape_zip_code(columns: bs4.element.ResultSet) -> str:
+    """Scrape the zip code from a row in the boliga sold table."""
     m = match_address(columns)
     zip_code = m.group('zip')
     return zip_code
 
 
 def make_request(street: str, zip_code: str) -> bs4.BeautifulSoup:
+    """Make request to boliga.dk."""
     url = (f'https://www.boliga.dk/salg/'
-       f'resultater?searchTab=1&propertyType=1&zipcodeFrom={zip_code}&'
-       f'zipcodeTo={zip_code}&street={urllib.parse.quote_plus(street)}&'
-       f'sort=date-d&page=1')
+           f'resultater?searchTab=1&propertyType=1&zipcodeFrom={zip_code}&'
+           f'zipcodeTo={zip_code}&street={urllib.parse.quote_plus(street)}&'
+           f'sort=date-d&page=1')
     response = requests.get(url)
     return bs4.BeautifulSoup(response.text, features="html.parser")
 
 
 def format_filename(streets_txt: str) -> str:
+    """Format the output csv file name."""
     name = Path(streets_txt).stem.lower().replace(' ', '_')
     return f'{name}.csv'
 
 
 def main(streets_txt: str, zip_code: str) -> None:
+    """Entry-point for script."""
     rows = []
     with open(streets_txt) as f:
         streets = f.read().splitlines()
@@ -165,7 +180,7 @@ if __name__ == '__main__':
     logformat = '%(filename)s:%(lineno)-3s :: %(levelname)-8s :: %(message)s'
     logging.basicConfig(format=logformat)
     logging.getLogger().setLevel(logging.INFO)
-    
+
     parser = argparse.ArgumentParser()
     parser.add_argument('streets_txt')
     parser.add_argument('zip')
